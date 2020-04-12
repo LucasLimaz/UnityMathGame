c801fee89e1952c061c7cdfd9c29a44deb18ebb5
breakyourfists <43384918+breakyourfists@users.noreply.github.com>
2020-04-12 19:25:03 -0300

    Correcao do bug de colisao.

---
+++ Assets/Scripts/ProblemaMatematico.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProblemaMatematico : MonoBehaviour
+{
+    public bool resolvido;
+    public string problema;
+    public int solucao;
+    public int[] solucoes;
+    public Sprite valor1, valor2, operador;
+    SpriteRenderer spriteRenderer;
+
+    public ProblemaMatematico(string problema, int solucao)
+    {
+        this.problema = problema;
+        this.solucao = solucao;
+        resolvido = false;
+        solucoes = new int[4];
+        for (int i = 0; i < solucoes.Length; i++)
+        {
+            solucoes[i] = 0;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = this.GetComponentInChildren<SpriteRenderer>();
+        spriteRenderer.sprite = valor1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

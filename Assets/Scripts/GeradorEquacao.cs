ebbc7f38fef90b2fb1153f01751ee83877542215
Blemish <39205389+xXblemishXx@users.noreply.github.com>
2020-04-14 23:05:45 -0300

    Background + gameObject Equacao

---
+++ Assets/Scripts/GeradorEquacao.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorEquacao : MonoBehaviour
+{
+
+    public GameObject geradorEquacao;
+    public float timeToCreate = 3f;
+    float clock = 3;
+    public float espacoEntreEquacao = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clock += Time.deltaTime;
+        if (clock >= timeToCreate)
+        {
+            Vector3 pos = transform.position;
+            pos.x = pos.x - Random.Range(-espacoEntreEquacao, espacoEntreEquacao);
+            clock = 0;
+            GameObject.Instantiate(geradorEquacao, pos, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/MoverEquacao.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverEquacao : MonoBehaviour
+{
+
+    ScoreManager score;
+    public float Speed = 1f;
+    bool isOn = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+        if(isOn == true)
+        { 
+            Vector3 pos = transform.position;
+            pos.y =     pos.y - Speed * Time.deltaTime;
+            transform.position = pos;
+
+            if(pos.y <= -475.8375)
+            {
+                GameObject.Destroy(gameObject);
+                GameObject.Find("ScoreManager").GetComponent<ScoreManager>().LoseLife();
+            }
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("floor"))
+        {
+            GameObject.Destroy(gameObject);
+            isOn = false;
+        }
+    }
+
+    public void DestruirObjeto()
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
